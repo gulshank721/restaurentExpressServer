ed0af04f940be64f3ef5af794c91f4a51e3f0c5a
Gulshan Kumar <gulshank721@gmail.com>
2023-01-08 15:47:29 +0530

    passport authentication

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promotionRouter = require('./routes/promotionRouter');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 var app = express();
 
 // database connection building
@@ -53,28 +56,25 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session())
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 //authentication
-function auth (req, res, next) {
-  console.log(req.session);
 
-if(!req.session.user) {
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 403;
-    return next(err);
-}
-else {
-  if (req.session.user === 'authenticated') {
-    next();
+    next(err);
   }
   else {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
+        next();
   }
-}
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
